ee19448734b04456821dbb56f53627d637552c32
xusuo <1901532726@qq.com>
2019-06-16 19:46:05 +0800

    Blog

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -1,15 +1,6 @@
 const mysql = require("mysql");
 
-function createConnection() {
-    const connection = mysql.createConnection({
-        host: "localhost",
-        port: "3306",
-        user: "root",
-        password: "root",
-        database: "my_blog"
-    });
-    return connection;
-}
+
 
 
 const pool = mysql.createPool({
@@ -42,5 +33,3 @@ let query = function (sql, values) {
 }
 
 module.exports.query = query
-
-module.exports.createConnection = createConnection;
